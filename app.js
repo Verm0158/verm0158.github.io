b5a774fbe8a629ee858a9b86769fe990c175f42d
Verm0158 <70522775+Verm0158@users.noreply.github.com>
2020-11-06 16:55:44 +0100

    Bug fixes

---
+++ app.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".navlinks");
+    const navLinks = document.querySelectorAll(".navlinks li")
+
+    burger.addEventListener("click",()=>{
+    // Toggle Nav
+        nav.classList.toggle("nav-active");
+    // Animate Links
+    navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.1}s`;
+            }
+        });
+        //Burger animation
+        burger.classList.toggle('toggle');
+
+    });
+}
+
+navSlide();
